2ca4748fd14886d2761effe95bc20509042892c8
GUILHERME MATHEUS RACCONE <guilherme_raccone@estudante.sc.senai.br>
2022-05-23 20:23:24 -0300

    branch 4

--- 6.c
+++ 6.c
@@ -4,3 +4,4 @@ int main(){
     printf("Atividade parte 1 ");
     return(0);
 }
+// ALTERAÇÃO BRANCH 4 
--- 7.c
+++ 7.c
@@ -4,3 +4,4 @@ int main(){
     printf("Atividade parte 2");
     return(0);
 }
+// ALTERAMENTO DE AGORA PDSFO0SIUQW-9U134R-98U4314134324324234234234
